a931adc2d67ccff6105ac249e980b13b48a172a8
Eduardo <jer@algoritmun.com>
2024-04-22 00:33:27 -0500

    Se agrega consulta del clima por ciudad

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -4,16 +4,42 @@ import Form from 'react-bootstrap/Form';
 
 const Homepage = () => {
 
-    const [formCity, setFormCity] = useState("")
+    const  API_WEATHER =  "https://api.weatherapi.com/v1/current.json?key=c21db2bcc653436281242558242204&q="
+    const [city, setCity] = useState("")
+    const [weather, setWeather] = useState({
+        city: "",
+        country: "",
+        temperature: 0,
+        condition: "",
+        conditionText: "",
+        icon: "",
+      });
     
-      const handleChange = (event: any) => {
-        setFormCity(event.target.value)
-      };
+    const handleChange = (event: any) => {
+        setCity(event.target.value)
+    };
     
-      const handleSubmit = (event: any) => {
+    const handleSubmit = async(event: any) => {
         event.preventDefault();
-        console.log(formCity);
-      };
+        try {
+            const res = await fetch(API_WEATHER + city);
+            const data = await res.json();
+
+            console.log(data);
+
+            setWeather({
+                city: data.location.name,
+                country: data.location.country,
+                temperature: data.current.temp_c,
+                condition: data.current.condition.code,
+                conditionText: data.current.condition.text,
+                icon: data.current.condition.icon,
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
     
     return (
         <div>
@@ -28,6 +54,21 @@ const Homepage = () => {
                     Submit
                 </Button>
             </Form>
+
+            {weather.city && (
+                <div>
+                    <h2>
+                        {weather.city}, {weather.country}
+                    </h2>
+                    <img alt={weather.conditionText} src={weather.icon} />
+                    <h3>
+                        {weather.temperature} °C
+                    </h3>
+                    <h5>
+                        {weather.conditionText} °C
+                    </h5>
+                </div>
+            )}
         </div>
     )
 };
